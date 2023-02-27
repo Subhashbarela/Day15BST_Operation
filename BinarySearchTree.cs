0af89fdd2a33c0545de4cad48d450bddf1ba7ff5
Subhash Barela <barelasubhash16@gmail.com>
2023-02-27 17:07:53 +0530

    create binarysearch tree

---
+++ BinarySearchTree.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BinarySearchTree
+{
+    internal class BinarySearchTree
+    {
+        public Node root;
+        public BinarySearchTree()
+        {
+           this. root = null;  
+        }
+        public void Insert(int data)
+        {
+            Node newNode = new Node(data);
+            if(root ==null)
+            {
+                root = newNode;
+            }
+            else
+            {
+                Node current= root;
+                Node parent;
+                while(true)
+                {
+                    parent= current;
+                    if(data<current.data)
+                    {
+                        current = current.left;
+                        if(current==null)
+                        {
+                            parent.left = newNode;
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        current = current.right;
+                        if(current == null)
+                        {
+                            parent.right = newNode;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        public void InorderTravalsal(Node node)
+        {
+            if(node !=null)
+            {
+                InorderTravalsal(node.left);
+                Console.WriteLine(node.data+" ");
+                InorderTravalsal(node.right);
+
+
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace BinarySearchTree
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        { 
+            BinarySearchTree bst= new BinarySearchTree();
+            bst.Insert(56);
+            bst.Insert(30);
+            bst.Insert(70);
+            bst.InorderTravalsal(bst.root);
+            Console.Read();
+        }
+    }
+}
